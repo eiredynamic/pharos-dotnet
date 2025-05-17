566be2280c667e8650fb98e2c35f415bbfe353bf
eiredynamic <18164717+eiredynamic@users.noreply.github.com>
2025-05-17 15:07:23 +0100

    Readded ProbeEventArgs which went missing upstream

---
+++ Eiredynamic.Pharos/ProbeEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Eiredynamic.Pharos;
+
+public class ProbeEventArgs<T> : EventArgs
+{
+    public T Event { get; }
+
+    public ProbeEventArgs(T eventInfo)
+    {
+        Event = eventInfo;
+    }
+}
